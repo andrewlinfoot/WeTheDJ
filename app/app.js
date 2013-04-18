92c320b144a271711c83b7cadf4fddd7dee09b35
Andrew Linfoot <ajlin500@yahoo.com>
2013-04-18 15:59:35 -0400

    added login system and applied UIs

--- app/app.js
+++ app/app.js
@@ -28,12 +28,101 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.home.greeting = function () {
-    if(Session.get('url')) {
-      return Session.get('url');
-    } else {
-      return "Welcome to app.";      
+  //Header Navigation
+  Template.header.events({
+    'click #_signup': function(e, t) {
+      e.preventDefault();
+      window.scrollTo(0,0);
+      Session.set('currentPage', 'signupPage');
+    },    
+    'click #_login': function(e, t) {
+      e.preventDefault();
+      window.scrollTo(0,0);
+      Session.set('currentPage', 'loginPage');
+    },    
+    'click #_home': function(e, t) {
+      e.preventDefault();
+      window.scrollTo(0,0);
+      Session.set('currentPage', 'homePage');
+    },
+    'click #_logout': function(e, t) {
+      e.preventDefault();
+      Meteor.logout( function(err) {
+        if (err) {
+          alert('error');
+        } else {
+          window.scrollTo(0,0);
+          Session.set('currentPage', 'homePage');
+        }
+      });
     }
+  });
+
+  //Sign Up Events
+  Template.signup.events({
+    'click #_login': function(e, t) {
+      e.preventDefault();
+      window.scrollTo(0,0);
+      Session.set('currentPage', 'loginPage');
+    },
+    'submit #createAccount, click #createAccount': function(e, t) {
+      e.preventDefault();
+
+      var email = t.find('#email').value
+        , password = t.find('#password').value
+        , confPassword = t.find('#confPassword').value;
+
+      if( email && password && confPassword ) {
+        if( !isValidEmail(email) ) {
+          alert("Not valid email");
+        } else if ( password != confPassword) {
+          alert("Passwords do not match");
+        } else {
+          Accounts.createUser({email: email, password: password}, function(error) {
+            if (error) {
+              alert("User create error");
+            }
+          });
+        }
+      } else {
+        alert("please enter all of the required fields");
+      }
+    }
+  });
+
+  //Login Events
+  Template.login.events({
+    'click #_signup': function(e, t) {
+      e.preventDefault();
+      window.scrollTo(0,0);
+      Session.set('currentPage', 'signupPage');
+    },
+    'submit #loginUser, click #loginUser': function(e, t) {
+      e.preventDefault();
+
+      var email = t.find('#email').value
+        , password = t.find('#password').value;
+
+      if( email && password ) {
+        if( !isValidEmail(email) ) {
+          alert("Not valid email");
+        } else {
+          Meteor.loginWithPassword(email, password, function(error) {
+            if (error) {
+              alert(error.reason);
+            }
+          });
+        }
+      } else {
+        alert("please enter all of the required fields");
+      }
+
+    }
+  });
+
+  var isValidEmail = function(email) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(email);
   };
 
   Template.home.events({
@@ -50,28 +139,55 @@ if (Meteor.isClient) {
     return true;
   };
 
+  Template.home.rendered = function() {
+      $(function() {
+        var Page = (function() {
+          var $navArrows = $( '#nav-arrows' ),
+            $nav = $( '#nav-dots > span' ),
+            slitslider = $( '#slider' ).slitslider( {
+              onBeforeChange : function( slide, pos ) {
 
+                $nav.removeClass( 'nav-dot-current' );
+                $nav.eq( pos ).addClass( 'nav-dot-current' );
 
-  // //Get Exchange URLs and store them in the Songs collection
-  // getExchangeURL = function(filePath) {
-  //   Meteor.call("SmartFile", "POST",
-  //     "/api/2/path/exchange/",
-  //     {auth: "2fm70a9TvdcItdhsQmwicn8THvnn61:mQnzKCvvAsILQDGmj20y6W1QSdNEkb",
-  //      params: {
-  //        path: filePath,
-  //        mode: "r",            //read only
-  //        expires: 60*60*24 }   //expires after 1 day     
-  //     },
-  //     function(error, results) {
-  //       console.log(results);
-  //       content = JSON.parse(results.content);
-  //       console.log(content);
-  //       Session.set('url',content.url);
+              }
+            } ),
+            init = function() {
+              initEvents();   
+            },
+            initEvents = function() {
+              // add navigation events
+              $navArrows.children( ':last' ).on( 'click', function() {
+                slitslider.next();
+                return false;
+              } );
+              $navArrows.children( ':first' ).on( 'click', function() {
+                slitslider.previous();
+                return false;
+              } );
+              $nav.each( function( i ) {
+                $( this ).on( 'click', function( event ) {
+                  var $dot = $( this );
+                  if( !slitslider.isActive() ) {
+                    $nav.removeClass( 'nav-dot-current' );
+                    $dot.addClass( 'nav-dot-current' );
+                  }
+                  slitslider.jump( i + 1 );
+                  return false;
+                } );
+              } );
+            };
+            return { init : init };
+        })();
+        Page.init();
+      });
 
-  //       //Meteor.call("insertSong", content);
-  //       Meteor.call("getSongList");
-  //   });
-  // }
+      $(document).ready(function () {
+        $('#shell img').plaxify()
+        $.plax.enable()
+      })
+
+  }
 }
 
 if (Meteor.isServer) {
